9e0811404e78dbfa202386a45a06aefb32a4989a
Stefan Fanaru <stefan.fanaru@outlook.com>
2024-05-23 11:07:00 +0300

    Add tmux meetings

--- tmux/scripts/next_meeting.sh
+++ tmux/scripts/next_meeting.sh
@@ -1,84 +1,71 @@
 #!/bin/bash
 CACHE_FILE="/tmp/meeting_cache.json"
-CACHE_DURATION=60 # Cache duration in seconds
+CACHE_DURATION=30 # Cache duration in seconds
 LOCK_FILE="$CACHE_FILE.lock"
 
-NERD_FONT_FREE="󱁕"
-NERD_FONT_MEETING="󰤙"
+NERD_FONT_FREE=""
+NERD_FONT_MEETING=""
 
-# Function to check if the cache is fresh
 is_cache_fresh() {
-    if [ ! -f "$CACHE_FILE" ]; then
-        return 1 # Cache doesn't exist
-    fi
-    local last_modified=$(date -r "$CACHE_FILE" +%s)
-    local current_time=$(date +%s)
-    local age=$((current_time - last_modified))
-    if [ "$age" -lt "$CACHE_DURATION" ]; then
-        return 0 # Cache is fresh
-    else
-        return 1 # Cache is stale
-    fi
+	if [ ! -f "$CACHE_FILE" ]; then
+		return 1 # Cache doesn't exist
+	fi
+	local last_modified=$(date -r "$CACHE_FILE" +%s)
+	local current_time=$(date +%s)
+	local age=$((current_time - last_modified))
+	if [ "$age" -lt "$CACHE_DURATION" ]; then
+		return 0 # Cache is fresh
+	else
+		return 1 # Cache is stale
+	fi
 }
 
-# Function to read from cache
 read_from_cache() {
-    cat "$CACHE_FILE"
+	cat "$CACHE_FILE"
 }
 
-# Function to fetch JSON from the specified URL
 fetch_json() {
-    curl -sk "https://localhost:7048/api/Meetings/next"
+	curl -sk "https://localhost:7048/api/Meetings/next"
 }
 
-# Function to write to cache
 write_to_cache() {
-    echo "$1" >"$CACHE_FILE"
+	echo "$1" >"$CACHE_FILE"
 }
 
-# Function to acquire lock
 acquire_lock() {
-    # Attempt to create the lock file, if it already exists, wait and try again
-    while true; do
-        if ln -s $$ "$LOCK_FILE" 2>/dev/null; then
-            return 0
-        fi
-        sleep 0.1
-    done
+	# Attempt to create the lock file, if it already exists, wait and try again
+	while true; do
+		if ln -s $$ "$LOCK_FILE" 2>/dev/null; then
+			return 0
+		fi
+		sleep 0.1
+	done
 }
 
-# Function to release lock
 release_lock() {
-    rm -f "$LOCK_FILE"
+	rm -f "$LOCK_FILE"
 }
 
-# Main script starts here
-
-# Acquire lock
 acquire_lock
 
-# Check if cache is fresh
 if is_cache_fresh; then
-    cached_result=$(read_from_cache)
-    echo "$cached_result"
-    # Release lock before exiting
-    release_lock
-    exit 0
+	cached_result=$(read_from_cache)
+	echo "$cached_result"
+	# Release lock before exiting
+	release_lock
+	exit 0
 fi
 
-# Fetch JSON
 json=$(fetch_json)
 
 # Parse JSON and extract time field
 time=$(echo "$json" | jq -r '.time')
 color=$(echo "$json" | jq -r '.color')
-
-# Extract the day of the meeting
-meeting_day=$(date -d "$time" +%Y-%m-%d)
+customText=$(echo "$json" | jq -r '.customText')
 
 # Calculate time difference in seconds
-current_time=$(date +%s)
-meeting_time=$(date -d "$time" +%s)
+current_time=$(date -u +%s)          # Current time in UTC
+meeting_time=$(date -ud "$time" +%s) # Meeting time in UTC
 time_diff=$((meeting_time - current_time))
 
 # Calculate hours and minutes
@@ -86,25 +73,25 @@ hours=$((time_diff / 3600))
 minutes=$(((time_diff % 3600) / 60))
 
 # Choose the icon based on the time left
-if [ "$hours" -eq 0 ] && [ "$minutes" -lt 15 ]; then
-    icon="$NERD_FONT_MEETING"
+if [ "$hours" -eq 0 ] && [ "$minutes" -lt 31 ]; then
+	icon="$NERD_FONT_MEETING"
 else
-    icon="$NERD_FONT_FREE"
+	icon="$NERD_FONT_FREE"
 fi
 
 result=""
-# Output the icon and time left
-if [ "$meeting_day" == "$(date -d 'tomorrow' +%Y-%m-%d)" ]; then
-    result="$icon;TMRW;$color"
-elif [ "$hours" -gt 0 ]; then
-    result=$(printf "%s;%02d:%02d;%s\n" $icon $hours $minutes $color)
+
+if [ -z "$customText" ]; then
+	if [ "$hours" -eq 0 ]; then
+		result=$(printf "%s;%dm;%s\n" $icon $minutes $color)
+	else
+		result=$(printf "%s;%dh %dm;%s\n" $icon $hours $minutes $color)
+	fi
 else
-    result="$icon;$(date -d @$meeting_time +'%H:%M');$color"
+	result="$icon;$customText;$color"
 fi
 
 write_to_cache "$result"
 echo "$result"
 
-# Release lock
 release_lock
-
---
+++ tmux/scripts/next_meeting_popup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+json=$(curl -sk "https://localhost:7048/api/Meetings/next")
+
+time_utc=$(jq -r '.time' <<<"$json")
+time_utc=$(jq -r '.time' <<<"$json")
+time_local=$(TZ="Europe/Bucharest" date -d "$time_utc UTC" +"%H:%M")
+date_local=$(TZ="Europe/Bucharest" date -d "$time_utc UTC" +"%Y-%m-%d")
+date_local_pretty=$(TZ="Europe/Bucharest" date -d "$time_utc UTC" +"%d %b")
+
+# Get the current date in Europe/Bucharest timezone
+current_date=$(TZ="Europe/Bucharest" date +"%Y-%m-%d")
+
+# Determine the format based on whether the event is today
+if [ "$date_local" == "$current_date" ]; then
+	starting_at="$time_local"
+else
+	starting_at="$time_local - $date_local_pretty"
+fi
+
+# Use jq to rename and reorder fields
+json_modified=$(jq --arg starting_at "$starting_at" '{
+  title: .title,
+  starting: $starting_at,
+  organizer: .organizer,
+  attendees: .attendees
+}' <<<"$json")
+
+result=$(echo "$json_modified" | yq -P eval | sed 's/^/    /')
+result=$(echo -e "\n$result\n")
+
+echo "$result" | bat --language=yaml --style=plain
+echo -e "\n"
+read -n 1 -s
