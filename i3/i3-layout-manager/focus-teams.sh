22749acf7aa55bb587a8cb1c4cc3ba499cddb75c
Stefan Fanaru <stefan.fanaru@outlook.com>
2024-06-22 20:55:49 +0300

    Improvements

---
+++ i3/i3-layout-manager/focus-teams.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+i3-msg "workspace number 1"
+i3-msg '[workspace="1" instance="crx_cifhbcnohmdccbgoicgdjpfamggdegmo"] focus'
--- i3/i3-layout-manager/move-teams-to-workspace1.sh
+++ i3/i3-layout-manager/move-teams-to-workspace1.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+current_workspace=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).num')
+
+if [ $current_workspace -eq 1 ]; then
+	i3-msg "workspace number 1"
+	i3-msg '[workspace="1" instance="crx_cifhbcnohmdccbgoicgdjpfamggdegmo"] focus'
+	i3-msg 'floating disable; move left; move right; move up'
+	exit
+fi
 i3-msg "workspace number 1"
-# focus right most window in workspace 1
 i3-msg '[class=".*"] focus; focus right; focus right; focus right'
 i3-msg 'split v'
 i3-msg "workspace number 3"
